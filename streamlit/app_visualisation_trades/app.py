8c54aa7dadb296a70c417cc7b44dd9194dbba981
Georges <92797658+georgrmat@users.noreply.github.com>
2025-01-21 22:18:30 +0100

    Update app.py

--- streamlit/app_visualisation_trades/app.py
+++ streamlit/app_visualisation_trades/app.py
@@ -3,11 +3,13 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-N = st.sidebar.slider("Nombre de trades", 1, 1000, 100)
+nb_trades_per_day = st.sidebar.slider("Nombre de trades par jour", 1, 50, 10)
+nb_days = st.sidebar.slider("Nombre de jours de simulation", 1, 1500, 100)
 win_rate = st.sidebar.slider("Win rate", 0, 100, 50)
-yield_win = st.sidebar.slider("Gain par trade gagnant", 0, 10, 3)
-yield_loss = st.sidebar.slider("Perte par trade perdant", -10, 0, -3)
+yield_win = st.sidebar.slider("Gain par trade gagnant", 0.0, 3.0, 1.0)
+yield_loss = st.sidebar.slider("Perte par trade perdant", -3.0, 0.0, -1.0)
 
+N = nb_trades_per_day * nb_days
 nb_winning_trades = int(N*win_rate/100)
 nb_losing_trades = N - nb_winning_trades
 
@@ -16,7 +18,6 @@ losses = yield_loss * np.ones(nb_losing_trades)
 
 trades = list(wins) + list(losses)
 random.shuffle(trades)
-sharpe = np.round(np.mean(trades) / np.std(trades), 2)
 
 evolution = 1 + np.array(trades)/100
 all_capital_evolution = [100000] + list(100000*evolution.cumprod())
@@ -28,4 +29,15 @@ ax.set_ylabel('Prix')
 ax.set_title('Evolution du capital')
 st.pyplot(fig)
 
-st.write(sharpe)
+init_capital = 100000
+sharpe = np.round(np.mean(trades) / np.std(trades), 2)
+sqn = sharpe * N
+kpis = {'capital_initial': init_capital,
+        'capital_final': final_capital,
+        'rendement': 100 * (final_capital - init_capital)/init_capital,
+        'sharpe': sharpe,
+        'sqn': sqn, 
+        'drawdown': 0}
+
+df_kpis = pd.DataFrame(kpis)
+st.dataframe(df_kpis)
