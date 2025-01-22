1ab2a59065d4e318d75e9155605051177d21fcb4
Georges <grahal.science@gmail.com>
2025-01-22 16:40:45 +0100

    utils folder

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-from kpi_functions import get_sharpe, get_sqn, get_max_drawdown
+from utils.kpi_functions import get_sharpe, get_sqn, get_max_drawdown
 
 
 nb_trades_per_day = st.sidebar.slider("Nombre de trades par jour", 1, 50, 10)
